4be190151cdd7001bcb8b29227dfd35df6d9d0d5
Tommy Nicholas <tomasienrbc@gmail.com>
2018-02-17 11:57:46 -0500

    Little config things

--- truffle.js
+++ truffle.js
@@ -7,6 +7,8 @@ let mnemonic = '';
 if (fs.existsSync('secrets.json')) {
   secrets = JSON.parse(fs.readFileSync('secrets.json', 'utf8'));
   ({ mnemonic } = secrets);
+} else {
+  mnemonic = 'candy maple cake sugar pudding cream honey rich smooth crumble sweet treat';
 }
 
 module.exports = {
@@ -16,6 +18,12 @@ module.exports = {
       network_id: '1',
       gas: 4500000,
       gasPrice: 10000000000,
+    },
+    development: {
+      provider: new HDWalletProvider(mnemonic, 'http://127.0.0.1:7545'),
+      network_id: '*',
+      gas: 4500000,
+      gasPrice: 20000000000,
     },
     rinkeby: {
       provider: new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io'),
